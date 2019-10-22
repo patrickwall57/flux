2a7f918653b8aaf0d94be47bc15ee5c625cee8ef
Todd Keech <tkeech1@hotmail.com>
2019-10-22 11:57:50 +0100

    Include config resource in fluxctl install
    This commit adds the ability to supply a config file to `fluxctl
    install`, which will get made into a config resource and mounted in to
    the fluxd deployment.
    
    Co-authored-by: Michael Bridgen <michael@weave.works>

--- pkg/install/go.mod
+++ pkg/install/go.mod
@@ -3,9 +3,16 @@ module github.com/fluxcd/flux/pkg/install
 go 1.13
 
 require (
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/cyphar/filepath-securejoin v0.2.2 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/instrumenta/kubeval v0.0.0-20190918223246-8d013ec9fc56
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749
 	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/tools v0.0.0-20200121210457-b3205ff6fffe // indirect
+	k8s.io/apimachinery v0.17.2 // indirect
+	k8s.io/helm v2.16.1+incompatible
 )
--- pkg/install/install_test.go
+++ pkg/install/install_test.go
@@ -1,13 +1,14 @@
 package install
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/instrumenta/kubeval/kubeval"
 	"github.com/stretchr/testify/assert"
 )
 
-func testFillInTemplates(t *testing.T, expectedManifestCount int, params TemplateParameters) {
+func testFillInTemplates(t *testing.T, expectedManifestCount int, params TemplateParameters) map[string][]byte {
 	manifests, err := FillInTemplates(params)
 	assert.NoError(t, err)
 	assert.Len(t, manifests, expectedManifestCount)
@@ -24,6 +25,7 @@ func testFillInTemplates(t *testing.T, expectedManifestCount int, params Templat
 			}
 		}
 	}
+	return manifests
 }
 
 func TestFillInTemplatesAllParameters(t *testing.T) {
@@ -40,7 +42,6 @@ func TestFillInTemplatesAllParameters(t *testing.T) {
 		AdditionalFluxArgs: []string{"arg1=foo", "arg2=bar"},
 		RegistryScanning:   true,
 	})
-
 }
 
 func TestFillInTemplatesMissingValues(t *testing.T) {
@@ -68,4 +69,71 @@ func TestFillInTemplatesNoMemcached(t *testing.T) {
 		GitLabel:    "label",
 		GitReadOnly: false,
 	})
+}
+
+func TestFillInTemplatesConfigFile(t *testing.T) {
+
+	configFile := `config1: configuration1
+config2: configuration2
+config3: configuration3`
+
+	tests := map[string]struct {
+		params              TemplateParameters
+		configFileName      string
+		configFileNameCheck string
+		deploymentFileCheck string
+	}{
+		"configMap": {
+			params: TemplateParameters{
+				GitURL:             "git@github.com:fluxcd/flux-get-started",
+				GitBranch:          "branch",
+				GitPaths:           []string{"dir1", "dir2"},
+				GitLabel:           "label",
+				GitUser:            "User",
+				GitEmail:           "this.is@anemail.com",
+				Namespace:          "flux",
+				ConfigAsConfigMap:  true,
+				AdditionalFluxArgs: []string{"arg1=foo", "arg2=bar"},
+			},
+			configFileName:      "flux-config.yaml",
+			configFileNameCheck: "    config2: config",
+			deploymentFileCheck: "name: flux-config",
+		},
+		"secret": {
+			params: TemplateParameters{
+				GitURL:             "git@github.com:fluxcd/flux-get-started",
+				GitBranch:          "branch",
+				GitPaths:           []string{"dir1", "dir2"},
+				GitLabel:           "label",
+				GitUser:            "User",
+				GitEmail:           "this.is@anemail.com",
+				Namespace:          "flux",
+				ConfigAsConfigMap:  false,
+				AdditionalFluxArgs: []string{"arg1=foo", "arg2=bar"},
+			},
+			configFileName: "flux-config.yaml",
+			// the following field value is the base64 encoding of the config file string above
+			configFileNameCheck: "  flux-config.yaml: Y29uZmlnMTogY29uZmlndXJhdGlvbjEKY29uZmlnMjogY29uZmlndXJhdGlvbjIKY29uZmlnMzogY29uZmlndXJhdGlvbjM=",
+			deploymentFileCheck: "secretName: flux-config",
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(*testing.T) {
+			configContent, err := ConfigContent(strings.NewReader(configFile), test.params.ConfigAsConfigMap)
+			if err != nil {
+				t.Fatal(err)
+			}
+			test.params.ConfigFileContent = configContent
+			manifests := testFillInTemplates(t, test.params)
+			for fileName, contents := range manifests {
+				if fileName == test.configFileName {
+					assert.Contains(t, string(contents), test.configFileNameCheck)
+				}
+				if fileName == "flux-deployment.yaml" {
+					assert.Contains(t, string(contents), test.deploymentFileCheck)
+				}
+			}
+		})
+	}
 }
